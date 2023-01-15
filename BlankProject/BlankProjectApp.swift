524f125228f325fddae2457adcfb958060e96911
Hollis Kwan <holliskwan.ios@gmail.com>
2023-01-15 11:29:42 -0800

    Initial Commit

---
+++ BlankProject/BlankProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlankProjectApp.swift
+//  BlankProject
+//
+//  Created by Hollis Kwan on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BlankProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
